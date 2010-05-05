0726eac7ff018a07de2481bb303b77a3ee975fcf
fivethreeo <devnull@localhost>
2010-05-05 14:52:14 +0200

    fixed error in MultilingualBlogEntriesMiddleware

--- cmsplugin_blog/middleware.py
+++ cmsplugin_blog/middleware.py
@@ -4,7 +4,7 @@ from cmsplugin_blog.models import Entry
 class MultilingualBlogEntriesMiddleware:
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if 'queryset' in view_kwargs and view_kwargs['queryset']-model == Entry:
+        if 'queryset' in view_kwargs and view_kwargs['queryset'].model == Entry:
             view_kwargs['queryset'] = view_kwargs['queryset'].filter(language=language)
             
 
