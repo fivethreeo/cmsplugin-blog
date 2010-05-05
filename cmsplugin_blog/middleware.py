fca4d8bf2ce257c57d09b5d27bd9bad35c10a3db
fivethreeo <devnull@localhost>
2010-05-05 14:51:08 +0200

    added MultilingualBlogEntriesMiddleware

---
+++ cmsplugin_blog/middleware.py
@@ -0,0 +1,10 @@
+from cms.utils import get_language_from_request
+from cmsplugin_blog.models import Entry
+
+class MultilingualBlogEntriesMiddleware:
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if 'queryset' in view_kwargs and view_kwargs['queryset']-model == Entry:
+            view_kwargs['queryset'] = view_kwargs['queryset'].filter(language=language)
+            
+
