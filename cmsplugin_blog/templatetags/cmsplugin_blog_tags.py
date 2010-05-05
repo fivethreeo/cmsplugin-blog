a1a09d6e0be13e32f6c2476aaae861b434a04e7a
fivethreeo <devnull@localhost>
2010-05-05 18:11:45 +0200

    added inclusion templatetag for month snippet

--- cmsplugin_blog/templatetags/cmsplugin_blog_tags.py
+++ cmsplugin_blog/templatetags/cmsplugin_blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from cms.utils import get_language_from_request
-from cmsplugin_blog.models import EntryTitle
+from cmsplugin_blog.models import Entry, EntryTitle
 
 register = template.Library()
 
@@ -12,3 +12,15 @@ def get_entry_title(entry, request):
         title = entry.entrytitle_set.all()[0]
     return title
 register.filter(get_entry_title)
+
+import datetime
+from django import template
+
+
+@register.inclusion_tag('cmsplugin_blog/month_links_snippet.html', takes_context=True)
+def render_month_links(context):
+    request = context["request"]
+    language = get_language_from_request(request)
+    return {
+        'dates': Entry.objects.filter(entrytitle__language=language).dates('pub_date', 'month'),
+    }
