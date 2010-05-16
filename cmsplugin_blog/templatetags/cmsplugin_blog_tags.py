4e5d0668d4a699779b56ad3ce1371d1f629e1f03
fivethreeo <devnull@localhost>
2010-05-17 00:15:16 +0200

    added render_tag_links templatetag / added template/ altered archive templates/ added translation of string in norwegian

--- cmsplugin_blog/templatetags/cmsplugin_blog_tags.py
+++ cmsplugin_blog/templatetags/cmsplugin_blog_tags.py
@@ -1,4 +1,8 @@
+import datetime
 from django import template
+
+from tagging.models import Tag
+
 from cms.utils import get_language_from_request
 from cmsplugin_blog.models import Entry, EntryTitle
 
@@ -9,5 +13,14 @@ def render_month_links(context):
     request = context["request"]
     language = get_language_from_request(request)
     return {
-        'dates': Entry.objects.filter(entrytitle__language=language).dates('pub_date', 'month'),
+        'dates': Entry.published.filter(entrytitle__language=language).dates('pub_date', 'month'),
+    }
+
+@register.inclusion_tag('cmsplugin_blog/tag_links_snippet.html', takes_context=True)
+def render_tag_links(context):
+    request = context["request"]
+    language = get_language_from_request(request)
+    filters = dict(is_published=True, pub_date__lte=datetime.datetime.now(), entrytitle__language=language)
+    return {
+        'tags': Tag.objects.usage_for_model(Entry, filters=filters)
     }
