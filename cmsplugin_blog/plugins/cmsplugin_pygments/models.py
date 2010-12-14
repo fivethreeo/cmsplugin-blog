4854e0f36e08e381791e0e3c25c20981d4396b67
fivethreeo <devnull@localhost>
2010-12-14 12:29:51 +0100

    removed pygments plugin

--- cmsplugin_blog/plugins/cmsplugin_pygments/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from cms.models import CMSPlugin
-
-class PygmentsPlugin(CMSPlugin):
-    code_language = models.CharField(max_length=20)
-    code = models.TextField()
