339f2d456499dc1461d9fa594550ca060d652cd7
fivethreeo <oyvind.saltvik@gmail.com>
2011-01-14 15:48:33 +0100

    missing urls

---
+++ cmsplugin_blog/test/testapp/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+)
+
+urlpatterns += patterns('',
+    url(r'^', include('cms.urls')),
+)
