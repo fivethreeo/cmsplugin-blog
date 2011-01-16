e2c6b126cd1bc2fdf08ea4719f8543ccb7efdfa4
fivethreeo <oyvind.saltvik@gmail.com>
2011-01-16 20:47:25 +0100

    add entry with one extra language in the admin

--- cmsplugin_blog/tests/__init__.py
+++ cmsplugin_blog/tests/__init__.py
@@ -1,8 +1,9 @@
 import datetime
 from django.core.urlresolvers import reverse
-from cmsplugin_blog.test.testcases import BaseBlogTestCase
-
 from django.contrib.auth.models import User
+
+from cmsplugin_blog.models import Entry
+from cmsplugin_blog.test.testcases import BaseBlogTestCase
 
 class BlogTestCase(BaseBlogTestCase):
     
@@ -15,8 +16,29 @@ class BlogTestCase(BaseBlogTestCase):
         title, entry = self.create_entry_with_title(published=True, 
             published_at=published_at)
         self.assertEquals(title.get_absolute_url(), '/en/test-page-1/%s/entry-title/' % published_at.strftime('%Y/%m/%d'))
-
-    def test_03_admin_change(self):
+        
+    def test_03_admin_add(self):
+        
+        superuser = User(username="super", is_staff=True, is_active=True, 
+            is_superuser=True)
+        superuser.set_password("super")
+        superuser.save()
+        
+        self.client.login(username='super', password='super')
+        
+        add_url = reverse('admin:cmsplugin_blog_entry_add')
+        
+        # edit english
+        response = self.client.get(add_url, {'language': 'en'})
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, 'language_button selected" id="debutton" name="en"' )
+        
+        # edit german
+        response = self.client.get(add_url, {'language': 'de'})
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, 'language_button selected" id="debutton" name="de"' )
+        
+    def test_04_admin_change(self):
         
         superuser = User(username="super", is_staff=True, is_active=True, 
             is_superuser=True)
@@ -41,3 +63,33 @@ class BlogTestCase(BaseBlogTestCase):
         response = self.client.get(edit_url, {'language': 'de'})
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, 'language_button selected" id="debutton" name="de"' )
+        
+    def test_05_admin_add_post(self):
+        
+        superuser = User(username="super", is_staff=True, is_active=True, 
+            is_superuser=True)
+        superuser.set_password("super")
+        superuser.save()
+        
+        self.client.login(username='super', password='super')
+        
+        add_url = reverse('admin:cmsplugin_blog_entry_add')
+        
+        # add english
+        response = self.client.post(add_url, {'language': 'en', 'title': 'english', 'slug': 'english',
+            'pub_date_0': '2011-01-16', 'pub_date_1': '09:09:09'})
+        # self.assertEquals(response.content, '')
+
+        self.assertEquals(response.status_code, 302)
+        
+        edit_url = reverse('admin:cmsplugin_blog_entry_change', args=(1,))
+
+        # add german
+        response = self.client.post(edit_url, {'language': 'de', 'title': 'german', 'slug': 'german',
+            'pub_date_0': '2011-01-16', 'pub_date_1': '09:09:09'})
+        self.assertEquals(response.status_code, 302)
+        
+        entry = Entry.objects.get(pk=1)
+        self.assertEquals([title.title for title in entry.entrytitle_set.all()], ['english', 'german'])
+        
+        
