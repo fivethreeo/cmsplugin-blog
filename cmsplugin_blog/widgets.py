5b6830776e77810d2af81f660d5be150eb32ba3a
fivethreeo <oyvind.saltvik@gmail.com>
2011-05-17 13:58:51 +0200

    make tags autocomplete work

--- cmsplugin_blog/widgets.py
+++ cmsplugin_blog/widgets.py
@@ -22,16 +22,48 @@ class AutoCompleteTagInput(forms.TextInput):
         tag_list = simplejson.dumps([tag.name for tag in page_tags],
                                     ensure_ascii=False)
         return output + mark_safe(u'''<script type="text/javascript">
-            blog.jQuery("#id_%s").autocomplete({
-                width: 150,
-                max: 10,
-                highlight: false,
-                multiple: true,
-                multipleSeparator: ", ",
-                scroll: true,
-                scrollHeight: 300,
-                matchContains: true,
-                autoFill: true,
-                source: %s
+            
+            var availableTags = %s
+            
+            function split( val ) {
+                return val.split( /,\s*/ );
+            }
+            function extractLast( term ) {
+                return split( term ).pop();
+            }
+            
+            blog.jQuery("#id_%s")
+                // don't navigate away from the field on tab when selecting an item
+                .bind( "keydown", function( event ) {
+                    if ( event.keyCode === $.ui.keyCode.TAB &&
+                            $( this ).data( "autocomplete" ).menu.active ) {
+                        event.preventDefault();
+                    }
+                })
+                .autocomplete({
+                    minLength: 0,
+                    source: function( request, response ) {
+                        // delegate back to autocomplete, but extract the last term
+                        response( $.ui.autocomplete.filter(
+                            availableTags, extractLast( request.term ) ) );
+                    },
+                    focus: function() {
+                        // prevent value inserted on focus
+                        return false;
+                    },
+                    select: function( event, ui ) {
+                        var terms = split( this.value );
+                        // remove the current input
+                        terms.pop();
+                        // add the selected item
+                        terms.push( ui.item.value );
+                        // add placeholder to get the comma-and-space at the end
+                        terms.push( "" );
+                        this.value = terms.join( ", " );
+                        return false;
+                    }
             });
-            </script>''' % (name, tag_list))
+
+
+
+            </script>''' % (tag_list, name))
