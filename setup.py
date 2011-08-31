6b0c264997b087523959701d63bc53cef65b845e
fivethreeo <oyvind.saltvik@gmail.com>
2011-08-31 14:03:34 +0200

    add cms req

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
     test_suite = "cmsplugin_blog.test.run_tests.run_tests",
     include_package_data=True,
     zip_safe=False,
-    install_requires=['simple-translation', 'djangocms-utils', 'django-tagging', 'django-staticfiles'],
+    install_requires=['django-cms', 'simple-translation', 'djangocms-utils', 'django-tagging', 'django-staticfiles'],
 )
