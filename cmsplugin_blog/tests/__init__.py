f28943b3cb7e9a566a2764a8d645484db2a9f675
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2011-09-05 03:46:41 +0300

    Edited cmsplugin_blog/tests/__init__.py via GitHub

--- cmsplugin_blog/tests/__init__.py
+++ cmsplugin_blog/tests/__init__.py
@@ -3,11 +3,38 @@ import datetime
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
-from cms.test_utils.util.context_managers import SettingsOverride
 
 from cmsplugin_blog.models import Entry
 from cmsplugin_blog.test.testcases import BaseBlogTestCase
 
+class NULL:
+    pass
+    
+class SettingsOverride(object):
+    """
+Overrides Django settings within a context and resets them to their inital
+values on exit.
+Example:
+with SettingsOverride(DEBUG=True):
+# do something
+"""
+    
+    def __init__(self, **overrides):
+        self.overrides = overrides
+        
+    def __enter__(self):
+        self.old = {}
+        for key, value in self.overrides.items():
+            self.old[key] = getattr(settings, key, NULL)
+            setattr(settings, key, value)
+        
+    def __exit__(self, type, value, traceback):
+        for key, value in self.old.items():
+            if value is not NULL:
+                setattr(settings, key, value)
+            else:
+                delattr(settings,key) # do not pollute the context!
+                
 class BlogTestCase(BaseBlogTestCase):
     
     def test_01_apphook_added(self):
