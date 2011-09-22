3aaca776a2b3292168594e4734c23336f28ddb88
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-23 00:04:59 +0200

    fix tagged_object_list in middleware

--- cmsplugin_blog/middleware.py
+++ cmsplugin_blog/middleware.py
@@ -1,4 +1,4 @@
-from simple_translation.middleware import MultilingualGenericsMiddleware
+from simple_translation.middleware import MultilingualGenericsMiddleware, filter_queryset_language
 from cmsplugin_blog.models import Entry
 
 class MultilingualBlogEntriesMiddleware(MultilingualGenericsMiddleware):
@@ -7,3 +7,8 @@ class MultilingualBlogEntriesMiddleware(MultilingualGenericsMiddleware):
         'django.middleware.locale.LocaleMiddleware',
         'cms.middleware.multilingual.MultilingualURLMiddleware'
     ]
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        super(MultilingualGenericsMiddleware, self).process_response(request, view_func, view_args, view_kwargs)
+        if 'queryset_or_model' in view_kwargs and hasattr(view_kwargs['queryset_or_model'], 'model'):
+            view_kwargs['queryset_or_model'] = filter_queryset_language(request, view_kwargs['queryset_or_model'])
