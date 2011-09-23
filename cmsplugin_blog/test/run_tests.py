c4271a77b8bb1a91a8bc9cec5b993b45c7267b5f
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-23 15:38:20 +0200

    test fix

--- cmsplugin_blog/test/run_tests.py
+++ cmsplugin_blog/test/run_tests.py
@@ -73,7 +73,7 @@ def run_tests():
         JQUERY_JS='',
         JQUERY_UI_JS='',
         STATIC_URL='/some/url/',
-        STATIC_ROOT=os.path.join(os.path.dirname(os.path.abspath(__file__)), os.dirpath, os.dirpath),
+        STATIC_ROOT=os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.pardir, os.path.pardir),
         TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner',
         TEST_OUTPUT_VERBOSE = True
     )
