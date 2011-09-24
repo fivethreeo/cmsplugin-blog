f30b99b468513e475a81c0a0ffa37b4bb4891705
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-24 19:32:03 +0200

    add utils

---
+++ cmsplugin_blog/utils.py
@@ -0,0 +1,14 @@
+from cms import settings
+from cms.middleware.multilingual import has_lang_prefix
+
+def is_multilingual():
+    return 'cmsplugin_blog.middleware.MultilingualBlogEntriesMiddleware' in settings.MIDDLEWARE_CLASSES
+
+def get_lang_name(lang):
+    return _(dict(settings.LANGUAGES)[lang])
+
+def add_current_root(url):
+    if is_multilingual() and not has_lang_prefix(url):
+        new_root = "/%s" % get_language()
+        url = new_root + url
+    return url
